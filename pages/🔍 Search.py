a87f1e8ba07b76566a18fa24b75ecfa1136c7259
BalbinaRaquel <balbina.rakell@outlook.com>
2024-07-16 20:04:32 -0300

    adjustments and improvements

---
+++ pages/🔍 Search.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+
+
+st.set_page_config(
+    page_title="Searching area",
+    page_icon="🔍"
+)
+
+st.header("Proceed your search &mdash;",  divider='rainbow')
